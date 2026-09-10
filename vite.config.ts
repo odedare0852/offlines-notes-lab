28b8f3ce094ab5faf774cd9813626604c8595c5b
Emmatron-blip <Chukwuemelieogu321@gmail.com>
2026-09-10 23:26:47 +0100

    Build Offline Notes Lab PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/offlines-noteslab/',
+  base: '/offlines-notes-lab/',
   plugins: [react()],
 })
